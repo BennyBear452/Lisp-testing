8edcb24df3852945ffa6bac32f49290e0d29dd6b
BennyBear452 <88167168+BennyBear452@users.noreply.github.com>
2023-10-19 17:58:18 -0500

    Create stack.py
    Gonna edit more later

---
+++ stack.py
@@ -0,0 +1,30 @@
+
+
+
+class Stack2:
+  topIndex
+  maxLen
+  stackRef = []
+  def __init__(Max):
+    self.Max=Max
+    maxLen = 99
+    topIndex = -1
+    stackRef= [100]
+    
+
+  def push(number):
+    if(topIndex == maxLen):
+      print("Error in push-stack is full")
+    else:
+      
+
+  def pop():
+    if(stackRef.isEmpty()):
+      print("Error in pop-stack is empty")
+    else:
+      topIndex-=1
+
+  def top():
+    if(stackRef.isEmpty()):
+      
+    
